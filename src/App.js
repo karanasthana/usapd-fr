a22c8f0f6e5c580e24ea5cc57d98f59b51885df9
Karan Asthana <karan.asthana1@gmail.com>
2021-10-22 23:58:00 -0400

    added new routes and a new library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LoginContainer from './Login/LoginContainer';
+import DashboardPage from './Dashboard/DashboardPage';
+import LandingPage from './Landing/LandingPage';
+import GraphPage from './Graphs/GraphPage';
+import SignupPage from './Signup/SignupPage';
+
+export default function App() {
+    // let isLoggedIn = true; // some logic
+    return (
+        <>
+            <Router>
+                <div>
+                    {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+                    <Switch>
+                        <Route path="/home">
+                            <LandingPage />
+                        </Route>
+                        <Route path="/dashboard">
+                            <DashboardPage />
+                        </Route>
+                        <Route exact  path="/signup">
+                            <SignupPage />
+                        </Route>
+                        <Route path="/trend/:id">
+                            <GraphPage />
+                        </Route>
+                        <Route path="/" render={()=> <LoginContainer/>} />
+                    </Switch>
+                </div>
+            </Router>
+        </>
+    );
 }
-
-export default App;
